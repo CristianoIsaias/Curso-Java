f0fd912ba65b6a257d7417304f87f0d9c6ce744b
Cristiano <crisano79@gmail.com>
2022-02-11 19:21:23 -0300

    importação

---
+++ AnteseDepois/src/testeTest.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class testeTest {
+
+	@Before
+	public void before() {
+		System.out.println("@before");
+	}
+	
+	@After
+	public void after() {
+		System.out.println("@after");
+	}
+	
+	@Test
+	public void test1() {
+		System.out.println("Teste 1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("Teste 2");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("Teste 3");
+	}
+
+
+}
